b86ea55ba24452f6c682c1ad3d3fac5c8de133f8
Mohammadmahdi Rubin <sheragin.r@gmail.com>
2026-09-23 19:13:44 +0330

    Fix issue label spacing in published articles

--- assets/articles-data.js
+++ assets/articles-data.js
@@ -54,7 +54,7 @@ window.renderRLSArticleList = function (containerId, lang) {
     var abstractText = a.abstract[lang] || a.abstract.en;
     var receivedLabel = lang === "fa" ? "دریافت" : lang === "ru" ? "Получено" : "Received";
     var onlineLabel = lang === "fa" ? "انتشار آنلاین" : lang === "ru" ? "Онлайн-публикация" : "Online publication";
-    var issueLabel = lang === "fa" ? "دوره ۱، شماره ۱" : lang === "ru" ? "Том 1, выпуск 1" : "Volume 1, Issue 1";
+    var issueLabel = lang === "fa" ? "دوره ۱&nbsp;، شماره ۱" : lang === "ru" ? "Том 1, выпуск 1" : "Volume 1, Issue 1";
     var viewLabel = lang === "fa" ? "مشاهده مقاله" : lang === "ru" ? "Открыть статью" : "View article";
     var doiText = a.doi ? "DOI: " + a.doi : lang === "fa" ? "DOI: هنوز اختصاص نیافته" : lang === "ru" ? "DOI: ещё не присвоен" : "DOI: Not yet assigned";
     return '<article class="article-record" data-article-id="' + a.id + '">' +
