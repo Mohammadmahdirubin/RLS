357296685b20d936281246987aefba65ffb1afc8
Mohammadmahdi Rubin <sheragin.r@gmail.com>
2026-09-25 23:54:53 +0330

    Optimize multilingual footer with permanent CC BY 4.0 license

--- assets/js/cc-license-footer.js
+++ assets/js/cc-license-footer.js
@@ -2,15 +2,14 @@ document.addEventListener('DOMContentLoaded', function () {
   const footer = document.createElement('footer');
   footer.className = 'site-footer cc-license-footer';
   footer.innerHTML = `
-    <div class="container">
-      <p>© 2026 RLS Journal</p>
-      <p><a href="license.html">Copyright &amp; License</a></p>
-      <p>
-        <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank" rel="noopener">
-          <img src="https://licensebuttons.net/l/by/4.0/88x31.png" alt="Creative Commons Attribution 4.0 International License">
-        </a>
-      </p>
-      <p>Articles are published under Creative Commons Attribution 4.0 International License (CC BY 4.0).</p>
+    <div class="container cc-footer-line">
+      <span>© 2026 RLS Journal |</span>
+      <a href="copyright.html">Copyright &amp; License</a>
+      <span>|</span>
+      <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank" rel="noopener" class="cc-license-link">
+        <img src="https://licensebuttons.net/l/by/4.0/88x31.png" alt="CC BY 4.0">
+      </a>
+      <span>Creative Commons Attribution 4.0 International (CC BY 4.0)</span>
     </div>`;
   document.body.appendChild(footer);
 });
